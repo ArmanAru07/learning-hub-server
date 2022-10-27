8ceb901582019e6caf1f7e4a67866e9d2517da21
ArmanAru <armanhosen768@gmail.com>
2022-10-27 22:09:17 +0600

    final

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 5004;
 app.use(cors());
 
 const courseinfo = require('./data/courses.json')
+const courseDetails = require('./data/courseDetails.json');
 
 app.get('/', (req, res) => {
     res.send('News API Running');
@@ -14,6 +15,12 @@ app.get('/', (req, res) => {
 app.get('/courseinfo', (req, res) =>{
     res.send(courseinfo);
 })
+
+app.get('/course-details/:id',(req, res) =>{
+    const id = req.params.id;
+    const selectedCourse = courseDetails.find(n => n.id === id);
+    res.send(selectedCourse);
+}) 
 
 app.listen(port, () => {
     console.log('learning hub server running')
