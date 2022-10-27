33f3b06f5e17ac9a576aed0c130550050a0bfc05
ArmanAru <armanhosen768@gmail.com>
2022-10-27 15:04:30 +0600

    dataLoad

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express();
-const port = process.env.PORT || 5000;
+const port = 5004;
+
+const courseinfo = require('./data/courses.json')
+// const courses = require('/data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('News API Running');
 })
+
+app.get('/courseinfo', (req, res) =>{
+    res.send(courseinfo);
+})
+
+// app.get('/courses-category', (req, res) => {
+//     res.send(courses)
+// })
 
 app.listen(port, () => {
     console.log('learning hub server running')
